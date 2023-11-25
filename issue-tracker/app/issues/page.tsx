53450a90d8f3db54cbb54804d34e703a3dfee8ec
Mohamed Ahmed <ma07041705@gmail.com>
2023-11-25 11:51:31 +0300

    $ set up radix Ui

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import {  Button } from '@radix-ui/themes';
 
 const IssuesPages = () => {
   return (
-    <div>IssuesPages</div>
+    <div>
+       <Button>New Issue</Button>
+    </div>
   )
 }
 
