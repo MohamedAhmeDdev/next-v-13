9997bce5552aa6b7649065f6bf2aa1d9b5b23b62
Mohamed Ahmed <ma07041705@gmail.com>
2023-11-25 15:04:27 +0300

    fetching data

--- issue-tracker/app/api/issues/route.ts
+++ issue-tracker/app/api/issues/route.ts
@@ -16,5 +16,12 @@ export async function POST(request: NextRequest){
     return NextResponse.json(newIssue, { status: 201 });
 }
 
+
+export async function GET(request: NextRequest) {
+    const issues = await prisma.issue.findMany();
+    return NextResponse.json(issues, { status: 200 });
+}
+
+
 //this a function for posting issues in the database
 //prisma is used to insert / create 
--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,9 +1,44 @@
+'use client'
 import {  Button } from '@radix-ui/themes';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const IssuesPages = () => {
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchIssues = async () => {
+      try {
+        const response = await axios.get('/api/issues');
+        setIssues(response.data);
+      } catch (error) {
+        setError('An unexpected error occurred.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchIssues();
+  }, []);
+
   return (
     <div>
+
+<div>
+      <h1>Issues</h1>
+      <ul>
+        {issues.map((issue) => (
+          <li key={issue.id}>
+            <strong>{issue.title}</strong>
+            <p>{issue.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+
        <Button><Link href='/issues/new'>New Issue</Link></Button>
     </div>
   )
